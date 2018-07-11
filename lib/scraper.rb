b14604281e275af4943a309afe4819cb2732d2bc
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-11 00:06:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,11 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
     html = open('./fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
